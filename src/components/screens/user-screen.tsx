0b260721c942c3a66f100b4b753b132bf562edc2
Atte Marttinen <attemarttinen@pop-os.localdomain>
2020-12-04 11:05:30 +0200

    Added functionality to own information

--- src/components/screens/user-screen.tsx
+++ src/components/screens/user-screen.tsx
@@ -4,18 +4,20 @@ import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { ReduxActions, ReduxState } from "../../store";
 import { KeycloakInstance } from "keycloak-js";
-import { AccessToken } from "../../types";
 import strings from "../../localization/strings";
 import { History } from "history";
-import { Tab, Tabs, WithStyles, withStyles } from "@material-ui/core";
+import { Tab, Tabs, WithStyles, withStyles, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Grid, GridDirection, GridProps, GridSize, IconButton, Typography, } from "@material-ui/core";
 import styles from "../../styles/components/screens/user-screen";
 import AppLayout from "../layouts/app-layout";
-
+import { AccessToken, OSMData } from "../../types";
 import ProductsTab from "../tabs/products-tab";
 import CategoryEditorTab from "../tabs/category-editor-tab";
 import MyInfoTab from "../tabs/my-info-tab";
+import { Category, Coordinates, Item, ItemProperty, LocationInfo, User } from "../../generated/client";
 
 import logo from "../../resources/images/toimintakeskus.png";
+import produce from "immer"
+import Api from "../../api/api";
 
 /**
  * Component props
@@ -31,12 +33,21 @@ interface Props extends WithStyles<typeof styles> {
 interface State {
   loading: boolean;
   tabIndex: number;
+  existingItem?: Item;
+  item?: Item;
+  dataChanged: boolean;
+  user?: User;
 }
 
 /**
  * Component for User screen
  */
 export class UserScreen extends React.Component<Props, State> {
+
+    /**
+   * Base path for Open Street Maps address search API
+   */
+  private osmAddressBasePath = "https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&polygon_svg=1&namedetails=1&countrycodes=fi&q=";
 
   /**
    * Constructor
@@ -47,15 +58,44 @@ export class UserScreen extends React.Component<Props, State> {
     super(props);
     this.state = {
       loading: true,
-      tabIndex: 0
+      tabIndex: 0,
+      dataChanged: false
     };
+  }
+
+  /**
+   * Default coordinates
+   */
+  private defaultCoordinates: Coordinates = {
+    latitude: 61.6877956,
+    longitude: 27.2726569
+  };
+
+    /**
+   * Component did mount life cycle method
+   */
+  public componentDidMount = async () => {
+    this.setState({ loading: true });
+    await this.fetchUserInformation();
+    this.setState({ loading: false });
+  }
+
+  /**
+   * Component did update life cycle method
+   */
+  public componentDidUpdate = async (prevProps: Props) => {
+    if (prevProps.signedToken === undefined && this.props.signedToken) {
+      this.setState({ loading: true });
+      await this.fetchUserInformation();
+      this.setState({ loading: false });
+    }
   }
 
   /**
    * Component render method
    */
   public render = () => {
-    const { tabIndex } = this.state;
+    const { tabIndex, user } = this.state;
     const { classes } = this.props;
 
     return (
@@ -80,13 +120,34 @@ export class UserScreen extends React.Component<Props, State> {
           <ProductsTab />
         }
         { tabIndex === 1 &&
-          <MyInfoTab />
+          <MyInfoTab
+            coordinates={ this.defaultCoordinates }
+            user={ user }
+          />
         }
         { tabIndex === 2 &&
           <CategoryEditorTab />
         }
       </AppLayout>
     );
+  }
+
+    /**
+   * Fetches user information
+   */
+  private fetchUserInformation = async () => {
+    const { signedToken } = this.props
+
+    if (!signedToken || !signedToken.userId) {
+      return;
+    }
+
+    const usersApi = Api.getUsersApi(signedToken);
+    const userId = signedToken.userId;
+
+    const user = await usersApi.findUser({ userId: userId })
+    
+    this.setState({ user: user })
   }
 
   /**
@@ -98,6 +159,81 @@ export class UserScreen extends React.Component<Props, State> {
   private setTabIndex = (event: React.ChangeEvent<{}>, newValue: number) => {
     this.setState({
       tabIndex: newValue
+    });
+  }
+
+ 
+  /**
+   * OSM API search function
+   *
+   * @param address address to search
+   * @returns response promise
+   */
+  private fetchNewCoordinatesForAddress = async (address?: string): Promise<Response> => {
+    return await fetch(`${this.osmAddressBasePath}${encodeURIComponent(address || "")}`);
+  }
+
+  /**
+   * Parses coordinates from OSM response
+   *
+   * @param response OSM response
+   * @returns coordinates promise
+   */
+  private parseCoordinates = async (response: Response): Promise<Coordinates> => {
+
+    const coordinates: Coordinates = { ...this.defaultCoordinates };
+
+    if (response.body === null) {
+      return coordinates;
+    }
+
+    const osmData: OSMData[] = await response.json();
+    if (osmData.length === 0) {
+      return coordinates;
+    }
+
+    const firstResult = osmData[0];
+    coordinates.latitude = Number(firstResult.lat);
+    coordinates.longitude = Number(firstResult.lon);
+    return coordinates;
+  }
+
+   /**
+   * Update item location info
+   *
+   * @param locationInfo location info
+   */
+  private updateLocationInfo = async (locationInfo: LocationInfo) => {
+    const { item } = this.state;
+
+    const previousLocationInfo = item?.metadata.locationInfo;
+    let newCoordinates: Coordinates = { ...this.defaultCoordinates };
+
+    if (previousLocationInfo?.address !== locationInfo.address) {
+      const response = await this.fetchNewCoordinatesForAddress(locationInfo.address);
+      const parsedCoordinates = await this.parseCoordinates(response);
+      newCoordinates = parsedCoordinates;
+    }
+
+    locationInfo.coordinates = newCoordinates;
+
+    this.setState(
+      produce((draft: State) => {
+        draft.dataChanged = true;
+        draft.item!.metadata.locationInfo = locationInfo;
+      })
+    );
+  }
+
+  /**
+   * Update user information
+   *
+   * @param properties properties
+   */
+  private updateProperties = (properties: ItemProperty[]) => {
+    this.setState({
+      dataChanged: true,
+      item: { ...this.state.item!, properties }
     });
   }
 }
--- src/components/tabs/my-info-tab.tsx
+++ src/components/tabs/my-info-tab.tsx
@@ -1,15 +1,149 @@
 import * as React from "react";
-
-import { withStyles, WithStyles, Typography } from "@material-ui/core";
-import styles from "../../styles/components/screens/add-item-screen";
+import styles from "../../styles/components/generic/item-form-dialog";
 import strings from "../../localization/strings";
+import { TextField, Typography, WithStyles, withStyles, Grid, GridDirection, GridProps, GridSize } from '@material-ui/core';
+import { gt } from "lodash";
+import Map from "../generic/map";
+import { LocationInfo, User } from "../../generated/client";
+import GenericButton from "../generic/generic-button"
+import produce from "immer";
+
+/**
+ * Interface describing component properties
+ */
+interface Props extends WithStyles<typeof styles> {
+
+  coordinates: any;
+  user?: User;
+}
 
 /**
  * Functional component for user information editing tab
  */
-const MyInfoTab: React.FC<WithStyles<typeof styles>> = () => {
+const MyInfoTab: React.FC<Props> = props => {
+  const {
+    classes,
+    coordinates,
+    user
+  } = props;
+
+
+
+
+  const renderTextField = (key: string, displayName: string, value: string) => {
+
+    return (
+      <TextField
+        key={ key }
+        label={ displayName }
+        value={ value }
+        className={ classes.propertyField }
+        variant="outlined"
+        size="medium"
+        fullWidth
+      />
+    )
+  };
+
+  /**
+   * Renders item column content
+   */
+  const renderMyInformationContent = () => {
+
+    return (
+      <>
+      <Grid
+        container
+        spacing={ 0 }
+        className={ classes.gridRoot }
+      >
+        <Grid
+          { ...getGridContainerProps( ) }
+        >
+          <Grid
+            { ...getGridItemProps(12, 6) }
+            className={ classes.column }
+          >
+            { renderTextField("name", strings.generic.name, user?.name || "") }
+            { user?.companyAccount && renderTextField("", "Y-Tunnus", user?.email || "" ) }
+            { renderTypography(strings.user.contactInformation) }
+            { renderTextField("name", strings.user.phoneNumber, user?.phoneNumber || "") }
+            { renderTextField("Y-Tunnus", strings.user.email, user?.email || "" ) }
+            <GenericButton
+              text={strings.user.changePassword}
+            />
+            <GenericButton 
+              text={strings.generic.save}
+            />
+          </Grid>
+          <Grid
+            { ...getGridItemProps(12, 6) }
+            className={ classes.column }
+          >
+            { user?.companyAccount && renderTextField("", strings.search.agency, "" || "" ) }
+            { renderTextField("address", strings.user.address, user?.address || "" ) }
+            { renderMap() }
+
+          </Grid>
+        </Grid>
+        </Grid>
+      </>
+    );
+  }
+
+   /**
+   * Method for setting container related props to grid components
+   *
+   * @param direction flex direction
+   * @returns grid properties
+   */
+  const getGridContainerProps = (direction?: GridDirection): GridProps => ({
+    container: true,
+    direction: direction || "row"
+  });
+
+  /**
+   * Method for setting item related props to grid components
+   *
+   * @param xs sizing for xs breakpoint
+   * @param md sizing for md breakpoint
+   * @returns grid properties
+   */
+  const getGridItemProps = (xs?: boolean | GridSize, md?: boolean | GridSize): GridProps => ({
+    item: true,
+    xs,
+    md
+  });
+
+  /**
+   * Renders map component
+   */
+  const renderMap = () => {
+
+    return (
+      <Map
+        locationInfo={ coordinates }
+      />
+    );
+  };
+
+  const renderTypography = (label: string) => {
+    
+    return (
+      <Typography
+              variant="h2"
+              className={ classes.propertyField }
+            >
+              { label }
+            </Typography>
+    )
+  }
+
+
   return (
-    <Typography variant="h2">{ strings.userPage.myInfo } -{ strings.comingSoon }</Typography>
+    <div>
+      { renderMyInformationContent() }
+    </div>
   );
 };
 
